c097b531eb2882f0b6822c9c64945409d8ca12ac
tslight <tslight@pm.com>
2021-12-18 23:02:30 +0000

    add flags aswell as interactive

--- GearInches.go
+++ GearInches.go
@@ -1,24 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func calculate(frontcog, rearcog, wheelsize float32) float32 {
+func interactive(prompt string) float64 {
+	fmt.Print(prompt, ": ")
+	var result float64
+	fmt.Scan(&result)
+	return result
+}
+
+func calculate(frontcog, rearcog, wheelsize float64) float64 {
+	if frontcog == 0 {
+		frontcog = interactive("Frontcog")
+	} else {
+		fmt.Println("Frontcog:", frontcog)
+	}
+	if rearcog == 0 {
+		rearcog = interactive("Rearcog")
+	} else {
+		fmt.Println("Rearcog:", rearcog)
+	}
+	if wheelsize == 0 {
+		wheelsize = interactive("Wheelsize")
+	} else {
+		fmt.Println("Wheelsize:", wheelsize)
+	}
 	return frontcog / rearcog * wheelsize
 }
 
 func main() {
-	var frontcog float32
-	var rearcog float32
-	var wheelsize float32
+	fmt.Println(`
+This program calculates gear inches given the number of teeth on the front cog,
+the number of teeth on the rear cog and the wheelsize.
 
-	fmt.Print("Frontcog size: ")
-	fmt.Scan(&frontcog)
-	fmt.Print("Rearcog size: ")
-	fmt.Scan(&rearcog)
-	fmt.Print("Wheelsize (inches): ")
-	fmt.Scan(&wheelsize)
+55" is the typical size for a BMX with either 44/16 or 25/9.
+`)
 
-	gearInches := calculate(frontcog, rearcog, wheelsize)
-	output := fmt.Sprintf("Gear inches = %.1f", gearInches)
+	frontcog := flag.Float64(
+		"frontcog", 0, "number of teeth on the front sprocket",
+	)
+	rearcog := flag.Float64(
+		"rearcog", 0, "number of teeth on the rear sprocket",
+	)
+	wheelsize := flag.Float64(
+		"wheelsize", 0, "diameter of rear wheel in inches",
+	)
+
+	flag.Parse()
+
+	gearInches := calculate(*frontcog, *rearcog, *wheelsize)
+
+	output := fmt.Sprintf("\nGear inches = %.1f\n", gearInches)
 	fmt.Println(output)
 }
