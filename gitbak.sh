353692aafa5eb8fcbc727d93896c8161c45dd18b
tslight <tslight@pm.com>
2025-12-01 23:36:27 +0000

    whitespace

--- gitbak.sh
+++ gitbak.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 readonly DATE="$(date +%Y-%m-%d)"
 
 [ -n "$1" ] && GITROOT="$1" || GITROOT="$HOME/src"
@@ -10,21 +11,21 @@ NAME="$(basename "$GITROOT")"
 [ -d "/tmp/$NAME-$DATE" ] || mkdir "/tmp/$NAME-$DATE"
 
 readarray -d '' REPOS < <(find "$GITROOT" \
-			       -maxdepth 10 \
-			       -type d \
-			       -name ".git" \
-			       -print0 | \
-			      sed 's/\/\.git//g')
+                               -maxdepth 10 \
+                               -type d \
+                               -name ".git" \
+                               -print0 | \
+                              sed 's/\/\.git//g')
 
 for repo in "${REPOS[@]}"; do
     name="$(basename "$repo")"
     zip -qr "/tmp/$NAME-$DATE/$name-$DATE.zip" "$repo" && \
-	echo "Sucessfully archived $name" || \
-	    echo "Oops. Something went wrong.."
+        echo "Sucessfully archived $name" || \
+            echo "Oops. Something went wrong.."
 done
 
 cd /tmp && zip -qr "$BACKUP_DEST/$NAME-$DATE.zip" "$NAME-$DATE" && \
     echo "Created $BACKUP_DEST/$NAME-$DATE.zip." ||
-	echo "Oops. Something went wrong.."
+        echo "Oops. Something went wrong.."
 
 rm -r "/tmp/$NAME-$DATE"
